9910e507871498d26566aa4b9ca855e3edf657d1
awaisabir <awaisabirqureshi@gmail.com>
2017-05-14 21:43:16 -0400

    Comments

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
+// imports
 import express from 'express'
 import rp from 'request-promise-native'
 import path from 'path'
 import bodyParser from 'body-parser'
 import CONFIG from './config/opeanweathermap'
 
+// app stuff
 const app = express()
 const PORT = 3000 || process.env.PORT
+
+// get API KEY
 const API_KEY = CONFIG.API_KEY
 
+// middleware && public folder
 app.use(bodyParser.json())
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
 
+
+// default route
 app.get('/', (req, res, next) => {
     res.sendFile(path.join(__dirname + '/public/html/index.html'))
 })
 
+// weather endpoint
 app.post('/weather', (req, res, next) => {
 
     const _lat = req.body.latitude
     const _lon = req.body.longitude
 
+    // send a request to the OpenWeatherMap API
     rp(`http://api.openweathermap.org/data/2.5/weather?lat=${_lat}&lon=${_lon}&appid=${API_KEY}&units=metric`)
         .then(response => JSON.parse(response))
         .then(data => res.json(data))
@@ -27,6 +36,7 @@ app.post('/weather', (req, res, next) => {
 
 })
 
+// fire up server
 app.listen(PORT, err => {
     if (err) return console.error(err)
 
