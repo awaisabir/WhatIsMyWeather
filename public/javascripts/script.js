af35e751530112a840d91ea432e394bef0024d6d
awaisabir <awaisabirqureshi@gmail.com>
2017-08-13 00:09:32 -0600

    Switched to fetch API :smirk:

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,9 +6,8 @@ let latitude = 0,
     longitude = 0
 
 /**
- *  Name: getLocation
- *  Purpose: Function to get the location from the browser, and populate the #weather.
- *               - Consumes the promise and poulates #weather based on result 
+ *  Name: getCurrentWeather
+ *  Purpose: Function make call to backend enpoint for current weather and populate the DOM 
  **/
 function getCurrentWeather() {
     getLocation().then(coords => {
@@ -20,7 +19,9 @@ function getCurrentWeather() {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({latitude, longitude}),
-        }).then(data => {
+        })
+        .then(response => response.json())
+        .then(data => {
             weather.innerHTML = `
                     Location: ${data.name}, ${data.sys.country}.<br/> 
                     <h1>${data.main.temp}&deg;C </h1>
@@ -28,28 +29,14 @@ function getCurrentWeather() {
                 `
             initMap(data.coord.lat, data.coord.lon)
         }).catch(err => console.log(err))
-
-
-        /* fetch(`/forecast/${coords.latitude}/${coords.longitude}`)
-                .then(response => response.json())
-                .then(json => console.log(json))
-                .catch(err => console.log(err))
-
-        getWeather('/weather', coords.latitude, coords.longitude)
-            .then(data => {
-                weather.innerHTML = `
-                    Location: ${data.name}, ${data.sys.country}.<br/> 
-                    <h1>${data.main.temp}&deg;C </h1>
-                    <h1>${toTitleCase(data.weather[0].description)}&nbsp;<img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png"/></h1>
-                `
-                initMap(data.coord.lat, data.coord.lon)
-            })
-            .catch(err => { weather.innerHTML = `Unexpected Error` })*/
     })
     .catch(err => { weather.innerHTML = `${err.message}` })
 }
 
-
+/**
+ *  Name: getForecast
+ *  Purpose: Function make call to backend enpoint for 5-day forecast and populate the DOM 
+ **/
 function getForecast(lat, long) {
     getLocation().then(coords => {
         fetch(`/forecast/${coords.latitude}/${coords.longitude}`)
@@ -60,6 +47,11 @@ function getForecast(lat, long) {
     .catch(err => { weather.innerHTML = `${err.message}` })
 }
 
+
+/**
+ *  Name: getLocation
+ *  Purpose: Function to get the location from the browser
+ **/
 function getLocation()
 {
     return new Promise((resolve, reject) => {
@@ -75,44 +67,9 @@ function getLocation()
 }
 
 /**
- *  Name: getWeather
- *  Purpose: Function to get the weather from the express back-end, via a XMLHttpRequest
- *  @param: url => endpoint to get weather from
- *          lat => latitude from the browser
- *          long => longitude from the browser
- *  @return: Promise with error or result
+ *  Name: initMap
+ *  Purpose: Function to initialize Google Map
  **/
-function getWeather(url, lat, long) {
-    return new Promise((resolve, reject) => {
-
-        let xhr = new XMLHttpRequest()
-        xhr.open('POST', url, true)
-        xhr.onload = function() {
-            if (this.status >= 200 && this.status < 300)
-                resolve(JSON.parse(xhr.response))
-            else {
-                reject({
-                    status: this.status,
-                    statusText: this.statusText
-                })
-            }
-        }
-
-        xhr.onerror = () => {
-            reject({
-                status: this.status,
-                statusText: this.statusText
-            })
-        }
-
-        xhr.setRequestHeader('Content-Type', 'application/json')
-        xhr.send(JSON.stringify({
-            latitude: lat,
-            longitude: long
-        }))
-    })
-}
-
 function initMap(lat, lng) {
     if (!lat || !lng) {
         let mapProp = {
@@ -136,6 +93,12 @@ function initMap(lat, lng) {
 
 }
 
+
+/**
+ *  Name: toTitleCase
+ *  @param: string
+ *  Purpose: Function to turn first letter of all words to uppercase
+ **/
 function toTitleCase(str)
 {
     return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()})
