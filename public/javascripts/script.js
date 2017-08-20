a643943955a440a5908ae4ac475eace243ec2f45
awaisabir <awaisabirqureshi@gmail.com>
2017-08-20 02:19:44 -0600

    Added support for weekly forecast

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,3 @@
-/**
- *  Variables
- **/
 let weather = document.getElementById('weather')
 let tab = document.querySelectorAll('.tabs ul li')
 let currentWeather = tab[0]
@@ -8,10 +5,7 @@ let fiveDayForecast = tab[1]
 let latitude = 0,
     longitude = 0
 
-/**
- *  Name: getCurrentWeather
- *  Purpose: Function make call to backend enpoint for current weather and populate the DOM 
- **/
+
 function getCurrentForecast() {
     fiveDayForecast.classList.remove('is-active')
     currentWeather.classList.add('is-active')
@@ -36,43 +30,21 @@ function getCurrentForecast() {
                         </header>
                         <div class="card-content">
                             <div class="content">
-                                <h1>${data.currently.temperature}&deg;C</h1>
+                                <canvas id="icon1" width="50" height="50"></canvas>
                                 <hr/>
                                 <small>
-                                    <h3>${data.currently.summary}</h3>
-                                    <canvas id="icon1" width="50" height="50"></canvas>
-                                </small>
-                                <hr/>
-                                <small>
-                                    <strong>Precipitation: </strong> ${data.currently.precipProbability*100}% <br/>                                
-                                    <strong>Humidity: </strong>${data.currently.humidity*100}% <br/>                                
+                                    <h1>${Math.ceil(data.currently.temperature)}&deg;C</h1>
+                                    <strong>Precip: </strong> ${data.currently.precipProbability*100}% <br/>                                
+                                    <strong>Humidity: </strong>${Math.round(data.currently.humidity*100)}% <br/>                                
                                     <strong>Wind: </strong>${data.currently.windSpeed} km/h <br/>
-                                    <strong>UV Index: </strong>;${data.currently.uvIndex} <br/>                                                                
+                                    <strong>UV Index: </strong>${data.currently.uvIndex} <br/>                                                                
                                 </small>
                                 <hr/>
-                                <small>
-                                    <h2>Alerts</h2>
-                                    <ul id="alerts"></ul>
-                                </small>
                             </div>
                         </div>
                     </div>
                 </div>
                 `
-                let alertsList = document.getElementById('alerts')
-                if (data.alerts === undefined) {
-                    alertsList.innerHTML = `<li>There are no alerts at this moment</li>`
-                } else {
-                    alertsList.innerHTML = ``
-                    for (alert of data.alerts) {
-                        alertsList.innerHTML += `
-                            <li>
-                                <h3>${alert.title}</h3>
-                                <p>${alert.description}</p>
-                            </li>
-                        `
-                    }
-                }
     
                 let skycons = new Skycons({"color": "#363636"})
                 skycons.set("icon1", data.currently.icon)
@@ -85,29 +57,54 @@ function getCurrentForecast() {
     .catch(err => weather.innerHTML = `${err.message}`)
 }
 
-/**
- *  Name: getForecast
- *  Purpose: Function make call to backend enpoint for 5-day forecast and populate the DOM 
- **/
 function getWeeklyForecast() {
     currentWeather.classList.remove('is-active')
     fiveDayForecast.classList.add('is-active')
+    addSpinner()
     getLocation()
     .then(coords => {
-        fetch(`/forecast/${coords.latitude}/${coords.longitude}`)
+        fetch(`/weekly/${coords.latitude}/${coords.longitude}`)
         .then(response => response.json())
-        .then(forecast => {
+        .then(weeklyForecast => {
+            weather.innerHTML = `<div class="display-weekly"></div>`
             
+            let display = document.getElementsByClassName('display-weekly')
+            let skycons = new Skycons({"color": "#363636"})            
+            weeklyForecast.shift()
+            let i = 0
+            for (forecast of weeklyForecast) {
+                let date = new Date(forecast.time*1000)
+                display[0].innerHTML += `
+                <div class="card">
+                    <header class="card-header">
+                        <p class="card-header-title">
+                            ${dayToString(date.getDay())}
+                        </p>
+                    </header>
+                    <div class="card-content">
+                        <div class="content">
+                            <canvas id="icon${i}" width="50" height="50"></canvas>
+                            <p>${forecast.summary}</p>
+                            <hr/>
+                            <small>
+                                <p><strong>Max:</strong> ${Math.ceil(forecast.apparentTemperatureMax)}&deg;C</p>
+                                <p><strong>Min:</strong> ${Math.ceil(forecast.apparentTemperatureMin)}&deg;C</p>
+                            </small>
+                            <hr/>
+                        </div>
+                    </div>
+                </div>
+                `
+                i++
+            }
+            addSkyCons(skycons, weeklyForecast)
+            skycons.play()
         })
         .catch(err => console.log(err))
     })
     .catch(err => { weather.innerHTML = `${err.message}` })
 }
 
-/**
- *  Name: getLocation
- *  Purpose: Function to get the location from the browser
- **/
 function getLocation() {
     return new Promise((resolve, reject) => {
         if (navigator.geolocation) {            
@@ -121,10 +118,6 @@ function getLocation() {
     })
 }
 
-/**
- *  Name: initMap
- *  Purpose: Function to initialize Google Map
- **/
 function initMap(lat, lng) {
     if (!lat || !lng) {
         let mapProp = {
@@ -174,11 +167,26 @@ function addSpinner() {
     `
 }
 
-/**
- *  Name: toTitleCase
- *  @param: string
- *  Purpose: Function to turn first letter of all words to uppercase
- **/
+function addSkyCons(skycons, weeklyForecast) {
+    skycons.add('icon0', weeklyForecast[0].icon)
+    skycons.add('icon1', weeklyForecast[1].icon)
+    skycons.add('icon2', weeklyForecast[2].icon)
+    skycons.add('icon3', weeklyForecast[3].icon)
+    skycons.add('icon4', weeklyForecast[4].icon)
+    skycons.add('icon5', weeklyForecast[5].icon)
+    skycons.add('icon6', weeklyForecast[6].icon)
+}
+
 function toTitleCase(str) {
     return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()})
+}
+
+function dayToString(int) {
+    if (int === 0) return 'Sun'
+    if (int === 1) return 'Mon'
+    if (int === 2) return 'Tue'
+    if (int === 3) return 'Wed'
+    if (int === 4) return 'Thu'
+    if (int === 5) return 'Fri'
+    if (int === 6) return 'Sat'
 }
