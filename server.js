7e0a2a984136f52524548471a21d7bb68b7548ec
awaisabir <awaisabirqureshi@gmail.com>
2017-08-20 16:39:11 -0600

    Setting up the React front end

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import bodyParser from 'body-parser'
 import CONFIG from './config/config'
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 
 const API_KEY = CONFIG.API_KEY
 
