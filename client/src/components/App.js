423ba4f79f3f2aafefbb0c6b91fc4cfe96c4da0e
awaisabir <awaisabirqureshi@gmail.com>
2017-08-21 21:54:21 -0600

    Added React Router to the project! :zap: :sparkles:

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,40 @@
-import React, { Component } from 'react';
-import logo from '../assets/logo.svg';
-import '../styles/App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+import CurrentForecast    from '../components/CurrentForecast'
+import WeeklyForecastList from '../components/WeeklyForecastList'
+
+import sun from '../assets/sun.svg'
+import cloudSun from '../assets/cloud-sun.svg'
+import '../styles/App.css'
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <h1>
+          <img src={sun} alt="cloud" width="50px" height="50px"/>
+          <strong>Weather</strong>
+          <img src={cloudSun} alt="cloud-sun" width="50px" height="50px"/>
+        </h1>
+
+        <div className="container">
+          <div className="tabs is-centered">
+            <ul>
+              <li><NavLink to="/current" activeStyle={{ color:'#00d1b2' }}>Current Forecast</NavLink></li>
+              <li><NavLink to="/weekly" activeStyle={{ color:'#00d1b2' }}>Weekly Forecast</NavLink></li>
+            </ul>
+          </div>
+          
+
+            <div>
+              <Route exact path="/current" component={CurrentForecast} />
+              <Route exact path="/weekly" component={WeeklyForecastList} />
+            </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
-    );
-  }
+    </Router>
+  )
 }
 
-export default App;
+export default App
