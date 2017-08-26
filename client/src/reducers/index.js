6853f875818874f98664e82ac507aa1e9461f71c
awaisabir <awaisabirqureshi@gmail.com>
2017-08-26 00:27:50 -0600

    Added new state keys & deleted useless action types

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 const initialState = {
-    fetching: false,
-    fetched: false,
     location: {},
+    fetching_location: false,
+    fetched_location: false,
     current_forecast: {},
     five_day_forecast: [],
-    errors: []
+    errors: [],
+    fetching_forecast: false,
+    fetched_forecast: false,
 }
 
 export default function reducer(state=initialState, action) {
@@ -12,44 +14,44 @@ export default function reducer(state=initialState, action) {
         case 'FETCH_GEO_LOCATION_PENDING':
             return {
                 ...state,
-                fetching: true,
+                fetching_location: true,
             }
 
         case 'FETCH_GEO_LOCATION_FULFILLED':
             return {
                 ...state,
-                fetching: false,
-                fetched: true,
+                fetching_location: false,
+                fetched_location: true,
                 location: {...action.payload},
             }
 
         case 'FETCH_GEO_LOCATION_REJECTED':
             return {
                 ...state,
-                fetching: false,
-                fetched: false,
+                fetching_location: false,
+                fetched_location: false,
                 errors: [...action.payload]
             }
         
         case 'FETCH_CURRENT_FORECAST_PENDING':
             return {
                 ...state,
-                fetching: true,
+                fetching_forecast: true,
             }
 
         case 'FETCH_CURRENT_FORECAST_FULFILLED':
             return {
                 ...state,
-                fetching: false,
-                fetched: true,
+                fetching_forecast: false,
+                fetched_forecast: true,
                 current_forecast: {...action.payload.data},
             }
 
         case 'FETCH_CURRENT_FORECAST_REJECTED':
             return {
                 ...state,
-                fetching: false,
-                fetched: false,
+                fetching_forecast: false,
+                fetched_forecast: false,
                 errors: [...action.payload.data]
             }
     
