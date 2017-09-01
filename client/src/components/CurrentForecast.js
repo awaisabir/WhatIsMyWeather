5459210914cfd88977cedfeb6fc4d2a996ca129f
awaisabir <awaisabirqureshi@gmail.com>
2017-08-31 20:22:54 -0600

    working on dispalying weekly forecast! :zap:

--- client/src/components/CurrentForecast.js
+++ client/src/components/CurrentForecast.js
@@ -1,50 +1,47 @@
-import React, { Component } from 'react'
+import React from 'react'
 import cloudSun from '../assets/cloud-sun.svg'
 
-class CurrentForecastComponent extends Component {
-    render() {
-        if (Object.keys(this.props.forecast).length === 0) {
-            return (
-                <div>
-                    <div className="spinner">
-                        <div className="double-bounce1"></div>
-                        <div className="double-bounce2"></div>
-                    </div>
-                    <h2>       
-                        Fetching weather ... 
-                        <img src={cloudSun} alt="cloud" width="25px" height="25px" />
-                    </h2>
-                </div>
-            )
-        }
-
-
+const CurrentForecastComponent = ({forecast}) => {
+  if (Object.keys(forecast).length === 0) {
     return (
-			<div style={{display: 'flex', justifyContent: 'space-around', marginBottom: '50px'}}>
-				<div className="card">
-					<header className="card-header">
-						<p className="card-header-title">
-							Location
-						</p>
-					</header>
-					<div className="card-content">
-						<div className="content">
-							{/* <canvas id="icon1" width="50" height="50"></canvas> */}
-							<hr/>
-							<small>
-								<h1>{Math.ceil(this.props.forecast.currently.apparentTemperature)}&deg;C</h1>
-								<strong>Precip: </strong> {this.props.forecast.currently.precipProbability*100}% <br/>                                
-								<strong>Humidity: </strong>{Math.round(this.props.forecast.currently.humidity*100)}% <br/>                                
-								<strong>Wind: </strong>{this.props.forecast.currently.windSpeed} km/h <br/>
-								<strong>UV Index: </strong>{this.props.forecast.currently.uvIndex} <br/>                                                                
-							</small>
-							<hr/>
-						</div>
-					</div>
-				</div>
-			</div>
-		)
-	}
+      <div>
+        <div className="spinner">
+          <div className="double-bounce1"></div>
+          <div className="double-bounce2"></div>
+        </div>
+        <h2>       
+          Fetching weather ... 
+          <img src={cloudSun} alt="cloud" width="25px" height="25px" />
+        </h2>
+      </div>
+    )
+  }
+
+  return (
+    <div style={{display: 'flex', justifyContent: 'space-around', marginBottom: '50px'}}>
+      <div className="card">
+        <header className="card-header">
+          <p className="card-header-title">
+            Location
+          </p>
+        </header>
+        <div className="card-content">
+          <div className="content">
+            {/* <canvas id="icon1" width="50" height="50"></canvas> */}
+            <hr/>
+            <small>
+              <h1>{Math.ceil(forecast.currently.apparentTemperature)}&deg;C</h1>
+              <strong>Precip: </strong> {forecast.currently.precipProbability*100}% <br/>                                
+              <strong>Humidity: </strong>{Math.round(forecast.currently.humidity*100)}% <br/>                                
+              <strong>Wind: </strong>{forecast.currently.windSpeed} km/h <br/>
+              <strong>UV Index: </strong>{forecast.currently.uvIndex} <br/>                                                             
+            </small>
+            <hr/>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default CurrentForecastComponent
--- client/src/components/WeeklyForecastList.js
+++ client/src/components/WeeklyForecastList.js
@@ -1,11 +1,48 @@
 import React from 'react'
+import cloudSun from '../assets/cloud-sun.svg'
 
 const WeeklyForecastList = ({forecast}) => {
+  if (Object.keys(forecast).length === 0) {
     return (
-        <div>
-            <h1>Weekly Forecast</h1>
+      <div>
+        <div className="spinner">
+          <div className="double-bounce1"></div>
+          <div className="double-bounce2"></div>
         </div>
+        <h2>       
+          Fetching weather ... 
+          <img src={cloudSun} alt="cloud" width="25px" height="25px" />
+        </h2>
+      </div>
     )
+  }
+
+  return (
+    <div style={{display: 'flex', justifyContent: 'space-around', marginBottom: '50px', flexWrap: 'wrap'}}>
+      {forecast.map(item => {
+          return (
+            <div className="card" style={{marginTop: '10px'}}>
+              <header className="card-header">
+                <p className="card-header-title">
+                  Location
+                </p>
+              </header>
+              <div className="card-content">
+                <div className="content">
+                  {/* <canvas id="icon1" width="50" height="50"></canvas> */}
+                  <hr/>
+                  <small>
+                    <h1>{item.summary}</h1>                                                         
+                  </small>
+                  <hr/>
+                </div>
+              </div>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
 }
 
 export default WeeklyForecastList
